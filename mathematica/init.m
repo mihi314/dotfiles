22a31b3e7c07af04c8a76e6e920838e75538e24d
Michael Engelhard <engelhard.michael+git@gmail.com>
2023-12-06 20:43:16 +0100

    Improve MakeConsts

--- mathematica/init.m
+++ mathematica/init.m
@@ -5,4 +5,15 @@
     Based on https://mathematica.stackexchange.com/questions/31708/creating-a-block-from-a-list-of-rules
     See also my "make const" mathematica notebooks.
 *)
-MakeConsts[rules_List] := Function[body, Block @@ Join[Apply[Set, Hold[rules], {2}], Hold[body]], HoldAll]
+SetAttributes[EvaluateOneStep, HoldAll]
+EvaluateOneStep[expr_] := Module[{P},
+    P = (P = Return[#, TraceScan] &) &;
+    TraceScan[P, expr, TraceDepth -> 1]
+]
+
+SetAttributes[MakeConsts, HoldAll]
+MakeConsts[rules_List] := Function[body, Block @@ Join[
+    Replace[Hold[rules], {(a_ -> b_) :> (a = b), (a_ :> b_) :> (a := b)}, {2}],
+    Hold[body]
+], HoldAll]
+MakeConsts[symbol_] := Function[body, EvaluateOneStep[symbol] /. _[x_] :> MakeConsts[x][body], HoldAll]
